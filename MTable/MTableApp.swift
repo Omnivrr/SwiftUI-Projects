b5e57973bce53b93b98c9f841f443942f3be8ba4
Omnivrr <Omnivrr@gmail.com>
2023-03-19 16:11:57 +0200

    Initial Commit

---
+++ MTable/MTableApp.swift
@@ -0,0 +1,17 @@
+//
+//  MTableApp.swift
+//  MTable
+//
+//  Created by Bukhari Sani on 19/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct MTableApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
