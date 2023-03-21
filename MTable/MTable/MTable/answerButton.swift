9b554fb26d7f18bb97f357fc0886eb34c497114e
Omnivrr <121700500+Omnivrr@users.noreply.github.com>
2023-03-21 04:51:38 +0200

    Add files via upload

---
+++ MTable/MTable/MTable/answerButton.swift
@@ -0,0 +1,28 @@
+//
+//  answerButton.swift
+//  MTable
+//
+//  Created by Bukhari Sani on 21/03/2023.
+//
+
+import SwiftUI
+
+struct answerButton: View {
+    var number : Int
+    var body: some View {
+        Text("\(number)")
+            .frame(width: 120, height: 90)
+            .font(.system(size: 40, weight: .bold))
+            .background(Color.blue)
+            .foregroundColor(Color.black)
+            .clipShape(RoundedRectangle(cornerRadius: 30))
+            .padding()
+    
+    }
+}
+
+struct answerButton_Previews: PreviewProvider {
+    static var previews: some View {
+        answerButton(number: 50)
+    }
+}
