0f9bea0d35df4a8858d89d2167e8db7fcaebbe61
Omnivrr <Omnivrr@gmail.com>
2023-03-29 05:09:49 +0300

    Added views

---
+++ Moonshot/GridLayout.swift
@@ -0,0 +1,64 @@
+//
+//  GridLayout.swift
+//  Moonshot
+//
+//  Created by Bukhari Sani on 29/03/2023.
+//
+
+import SwiftUI
+
+struct GridLayout: View {
+    let astronauts: [String: Astronaut]
+    let missions: [Mission]
+    
+    let columns = [
+        GridItem(.adaptive(minimum: 150))]
+    
+    
+    var body: some View {
+            ScrollView {
+                LazyVGrid(columns: columns) {
+                    ForEach(missions) { mission in
+                        NavigationLink {
+                            MissionView(mission: mission, astronauts: astronauts)
+                        } label: {
+                            VStack {
+                                Image(mission.image)
+                                    .resizable()
+                                    .scaledToFit()
+                                    .frame(width: 100, height: 150)
+                                    .padding()
+                                
+                                VStack {
+                                    Text(mission.displayName)
+                                        .font(.headline)
+                                        .foregroundColor(.white)
+                                    
+                                    Text(mission.formattedlaunchDate)
+                                        .font(.caption)
+                                        .foregroundColor(.white.opacity(0.5))
+                                }
+                                .padding(.vertical)
+                                .frame(maxWidth: .infinity)
+                                .background(.lightBackground)
+                                
+                            }
+                            .clipShape(RoundedRectangle(cornerRadius: 10))
+                            .overlay(
+                                RoundedRectangle(cornerRadius: 10)
+                                    .stroke(.lightBackground)
+                            )
+                            
+                        }
+                    }
+                }
+            }
+        }
+    }
+    
+    struct GridLayout_Previews: PreviewProvider {
+        static var previews: some View {
+            GridLayout(astronauts: Bundle.main.decode("astronauts.json"), missions: Bundle.main.decode("missions.json"))
+                .preferredColorScheme(.dark)
+        }
+    }
