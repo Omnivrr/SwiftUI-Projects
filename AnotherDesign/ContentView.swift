f6867198ee240709b54b27a591a976c77accdfbd
Omnivrr <Omnivrr@gmail.com>
2023-04-05 03:49:59 +0300

    Initial Commit

---
+++ AnotherDesign/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  AnotherDesign
+//
+//  Created by Bukhari Sani on 05/04/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
