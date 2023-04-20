e696a0bb6793b88cc5b7255343ea0e68be0ab36f
Omnivrr <Omnivrr@gmail.com>
2023-04-20 16:05:04 +0300

    Added animation

--- Arrow/ContentView.swift
+++ Arrow/ContentView.swift
@@ -8,9 +8,21 @@ import SwiftUI
 
 // Define a custom shape called "Arrow" that conforms to the Shape protocol
 struct Arrow: Shape {
+    var headHeight = 0.5
+    var shaftWidth = 0.5
+    
+    var animatedData: AnimatablePair<Double, Double> {
+        get { AnimatablePair(headHeight, shaftWidth) }
+        set {
+            headHeight = newValue.first
+            shaftWidth = newValue.second
+        }
+    }
     
     // This method is required by the Shape protocol, and returns a Path object
     func path(in rect: CGRect) -> Path {
+        let height = rect.height * headHeight
+        let thickness = rect.width * shaftWidth / 2
         
         // Create a new empty Path object
         return Path { path in
@@ -19,19 +31,19 @@ struct Arrow: Shape {
             path.move(to: CGPoint(x: rect.midX, y: 0))
             
             // Add a line from the current point to the center of the right edge of the rectangle
-            path.addLine(to: CGPoint(x: rect.maxX, y: rect.midY))
+            path.addLine(to: CGPoint(x: rect.maxX, y: height))
             
             // Add a line from the current point to the point 50 units to the right of the center of the rectangle
-            path.addLine(to: CGPoint(x: rect.midX + 50, y: rect.midY))
+            path.addLine(to: CGPoint(x: rect.midX + thickness, y: height))
             
             // Add a line from the current point to the bottom edge of the rectangle, 50 units to the right of the center
-            path.addLine(to: CGPoint(x: rect.midX + 50, y: rect.maxY))
+            path.addLine(to: CGPoint(x: rect.midX + thickness, y: rect.maxY))
             
             // Add a line from the current point to the bottom edge of the rectangle, 50 units to the left of the center
-            path.addLine(to: CGPoint(x: rect.midX - 50, y: rect.maxY))
+            path.addLine(to: CGPoint(x: rect.midX - thickness, y: rect.maxY))
             
             // Add a line from the current point to the point 50 units to the left of the center of the rectangle
-            path.addLine(to: CGPoint(x: rect.midX - 50, y: rect.midY))
+            path.addLine(to: CGPoint(x: rect.midX - thickness, y: rect.midY))
             
             // Add a line from the current point back to the center of the top edge of the rectangle, completing the shape
             path.addLine(to: CGPoint(x: 0, y: rect.midY))
@@ -42,15 +54,25 @@ struct Arrow: Shape {
     }
 }
 
+
+
 // Define a SwiftUI view called ContentView
 struct ContentView: View {
+    @State private var headHeight = 0.5
+    @State private var shaftWidth = 0.25
     
     // The body property is required by the View protocol, and returns the view hierarchy
     var body: some View {
+        Arrow(headHeight: headHeight, shaftWidth: shaftWidth)
+            .fill(.blue)
+            .onTapGesture {
+                headHeight = Double.random(in: 0.2...0.8)
+                shaftWidth = Double.random(in: 0.2...0.8)
+            }
         
         // Display an instance of the Arrow shape with a purple fill
         Arrow()
-            .fill(.purple)
+            .fill(.blue)
     }
 }
 
