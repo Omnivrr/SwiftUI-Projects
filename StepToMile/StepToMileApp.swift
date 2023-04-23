07d4faaf7ccb6fda38601bc9f99d57e544b92dcb
Omnivrr <Omnivrr@gmail.com>
2023-04-24 00:49:43 +0300

    Initial Commit

---
+++ StepToMile/StepToMileApp.swift
@@ -0,0 +1,17 @@
+//
+//  StepToMileApp.swift
+//  StepToMile
+//
+//  Created by Bukhari Sani on 24/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct StepToMileApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
